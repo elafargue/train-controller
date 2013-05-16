14688a65d1ef31fb19c7e370c9c19a07180dd36d
Edouard Lafargue <edouard@lafargue.name>
2013-05-15 19:41:15 -0700

    Make graph axes the same color as the lines
    Now the user can understand which graph axis matches which line...

--- server/public/js/views/loco/locorun.js
+++ server/public/js/views/loco/locorun.js
@@ -26,6 +26,10 @@ window.LocoRunView = Backbone.View.extend({
         // Create a timer that updates the running time while power is above zero
         // (cleared when view is removed)
         this.timer = setInterval(this.updateRuntime.bind(this), 1000);
+        
+        // My own nice color palette:
+        this.palette = ["#e27c48", "#acbe80",  "#f1ca4f",  "#77b1a7", "#858485", "#d9c7ad", "#5a3037", ],
+
         this.render();
     },
     
@@ -84,21 +88,78 @@ window.LocoRunView = Backbone.View.extend({
     // We can only add the plot once the view has finished rendering and its el is
     // attached to the DOM, so this function has to be called from the home view.
     addPlot: function() {
+        var self = this;
         // Now initialize the plot area:
         var options = {
             series: { shadowSize: 0 }, // drawing is faster without shadows
-            yaxes: [ {  min:0, max:3300 },
-                     {position: "right", alignTicksWithAxis: 1 , min:0, max:900},
-                     { min:0, max: 1000}
+            yaxes: [ {  min:0, max:3300, font: {color: this.palette[0]} },
+                     {position: "right", alignTicksWithAxis: 1 , min:0, max:900, font: {color: this.palette[1]}},
+                     { min:0, max: 1000, font: {color: this.palette[2]}}
                    ],
             xaxes: [ { show: false } ],
-            legend: { position: "ne" }
+            legend: { position: "ne" },
+//            crosshair: {
+//				mode: "x"
+//			},
+//            grid: {
+//				hoverable: true,
+//				autoHighlight: false
+//			},
+            colors: this.palette,
         };
-        console.log('Loco chart size: ' + this.$('.locochart').width());
-        this.plot = $.plot($(".locochart", this.el), [ { data:this.packData(this.bemf), label: "RPM" },
-                                                      { data:this.packData(this.rate), label: "Power", yaxis: 2},
-                                                      { data:this.packData(this.current), label:"Current", yaxis: 3},
+        console.log('Loco chart size: ' + $('.locochart').width());
+        this.plot = $.plot($('.locochart', this.el), [ { data:this.packData(this.bemf), label: "RPM (mV)" },
+                                                      { data:this.packData(this.rate), label: "Power (%)", yaxis: 2},
+                                                      { data:this.packData(this.current), label:"Current (mA)", yaxis: 3},
                                                       { data:this.packData(this.targetbemf), color: "rgba(127,127,127,0.3)" }], options);
+        
+//        var legends = $(".locochart .legendLabel", this.el);
+//        var updateLegendTimeout = null;
+//        var latestPosition = null;
+//	    function updateLegend() {
+//           updateLegendTimeout = null;
+//           var pos = latestPosition;
+//
+//			var axes = self.plot.getAxes();
+//			if (pos.x < axes.xaxis.min || pos.x > axes.xaxis.max ||
+//				pos.y < axes.yaxis.min || pos.y > axes.yaxis.max) {
+//				return;
+//			}
+//
+//			var i, j, dataset = self.plot.getData();
+//			for (i = 0; i < dataset.length; ++i) {
+//				var series = dataset[i];
+//				// Find the nearest points, x-wise
+//				for (j = 0; j < series.data.length; ++j) {
+//					if (series.data[j][0] > pos.x) {
+//						break;
+//					}
+//				}
+//				// Now Interpolate
+//				var y,
+//					p1 = series.data[j - 1],
+//					p2 = series.data[j];
+//
+//				if (p1 == null) {
+//					y = p2[1];
+//				} else if (p2 == null) {
+//					y = p1[1];
+//				} else {
+//					y = p1[1] + (p2[1] - p1[1]) * (pos.x - p1[0]) / (p2[0] - p1[0]);
+//				}
+//                if (typeof(series.label) != 'undefined')
+//                    legends.eq(i).text(series.label.replace(/=.*/, "= " + y.toFixed(2)));
+//			}
+//
+//		}
+//
+//		$('.locochart', this.el).bind("plothover",  function (event, pos, item) {
+//			latestPosition = pos;
+//			if (!updateLegendTimeout) {
+//				updateLegendTimeout = setTimeout(updateLegend, 50);
+//			}
+//		});
+
     },
     
     packData: function(table) {
@@ -138,9 +199,9 @@ window.LocoRunView = Backbone.View.extend({
                 this.targetbemf.push(targetVal);
                 this.current = this.current.slice(1);
                 this.current.push(currentVal);
-                this.plot.setData([ { data:this.packData(this.bemf), label: "RPM" },
-                                    { data:this.packData(this.rate), label: "Power", yaxis: 2},
-                                    { data:this.packData(this.current), label:"Current", yaxis: 3},
+                this.plot.setData([ { data:this.packData(this.bemf), label: "RPM=0" },
+                                    { data:this.packData(this.rate), label: "Power=0", yaxis: 2},
+                                    { data:this.packData(this.current), label:"Current=0", yaxis: 3},
                                     { data:this.packData(this.targetbemf), color: "rgba(127,127,127,0.3)" }]);
                 this.plot.draw();
             }
